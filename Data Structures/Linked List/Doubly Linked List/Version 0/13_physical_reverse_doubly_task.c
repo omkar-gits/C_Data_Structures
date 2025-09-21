a82838eebf50d8c7fcbed79f4a9899ba7a3e0727
omkar-gits <sangpal.omkar2811@gmail.com>
2025-09-21 22:01:20 +0530

    Doubly Linked List completed..

---
+++ Data Structures/Linked List/Doubly Linked List/Version 0/13_physical_reverse_doubly_task.c
@@ -0,0 +1,92 @@
+#include<stdio.h>
+#include<malloc.h>
+
+struct node{
+    struct node*pPrev;
+    int iData;
+    struct node*pNext;
+};
+
+void InsertFirst(struct node** , int);
+void Display(struct node*);
+void DeleteAll(struct node**);
+void PhysicalReverse(struct node**);
+
+int main(void){
+    struct node *pFirst = NULL;
+    InsertFirst(&pFirst , 50);
+    InsertFirst(&pFirst , 40);
+    InsertFirst(&pFirst , 30);
+    InsertFirst(&pFirst , 20);
+    InsertFirst(&pFirst , 10);
+    Display(pFirst);
+    PhysicalReverse(&pFirst);
+    Display(pFirst);
+    DeleteAll(&pFirst);
+    return 0;
+}
+
+void InsertFirst(struct node**ppHead , int iNO){
+    struct node*pNewNode = NULL;
+    pNewNode = (struct node*)malloc(sizeof(struct node));
+    if(NULL == pNewNode){
+        printf("Memory Allocation Failed..\n");
+        return;
+    }
+
+    pNewNode->iData = iNO;
+    pNewNode->pPrev = NULL;
+    if(NULL == *ppHead){
+        pNewNode->pNext = NULL;
+        *ppHead = pNewNode;
+        return;
+    }
+    pNewNode->pNext = *ppHead;
+    (*ppHead)->pPrev = pNewNode;
+    *ppHead = pNewNode;
+
+}
+
+void Display(struct node*pHead){
+    if(NULL == pHead){
+        printf("The List is empty..!\n");
+        return;
+    }
+    printf("NULL");
+    while(pHead != NULL){
+        printf("<-|%d|->", pHead->iData);
+        pHead = pHead->pNext;
+    }
+    printf("NULL\n");
+}
+
+void DeleteAll(struct node** ppHead){
+    struct node * pTemp = NULL;
+    if(NULL == *ppHead){
+        return;
+    }
+    while(*ppHead != NULL){
+        pTemp = *ppHead;
+        *ppHead = pTemp->pNext;
+        free(pTemp);
+    }
+    return;
+}
+
+void PhysicalReverse(struct node** ppHead){
+    struct node* pPrev = NULL;
+    struct node* pCurrent = *ppHead;
+    //struct node* pNext = NULL;
+
+    if(NULL == *ppHead){
+        return;
+    }
+
+    while(NULL != pCurrent){
+        pCurrent->pPrev = pCurrent->pNext;
+        pCurrent->pNext = pPrev;
+        pPrev = pCurrent;
+        pCurrent = pCurrent->pPrev;
+    }
+    *ppHead = pPrev;
+}
