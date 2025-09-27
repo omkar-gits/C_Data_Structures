658141cbd4972c1bf5abfa55d7215d9163f777ca
omkar-gits <sangpal.omkar2811@gmail.com>
2025-09-28 00:46:56 +0530

    Update

--- Data Structures/Linked List/Singly Circular Linked List/Version 1/Singly_Circular_Linked_List_Version_1.c
+++ Data Structures/Linked List/Singly Circular Linked List/Version 1/Singly_Circular_Linked_List_Version_1.c
@@ -7,9 +7,22 @@ struct node{
 };
 
 void InsertFirst(struct node** , struct node**, int);
-void InsertFirst(struct node** , struct node**, int);
+void InsertLast(struct node** , struct node**, int);
+int DeleteFirst(struct node** , struct node**);
+int DeleteLast(struct node** , struct node**);
 void Display(struct node* ,struct node*);
+int CountNodes(struct node* , struct node*);
+int SearchFirstOccurrence(struct node* , struct node* , int);
+int SearchLastOccurrence(struct node* , struct node* , int);
+int SearchLastOccurrence(struct node* , struct node* , int);
+void InsertAtPosition(struct node** , struct node** , int);
+int DeleteAtPosition(struct node** , struct node**);
+void ConcatLists(struct node** , struct node**);
+void ConcatAtPosition(struct node** , struct node** , int);
+void PhysicalReverse(struct node** , struct node**);
+void ReverseDisplay(struct node*);
 void DeleteAll(struct node** , struct node**);
+
 
 int main(void){
     struct node* pFirst = NULL;
@@ -28,10 +41,25 @@ int main(void){
     InsertLast(&pFirst , &pLast , 500);
 
     Display(pFirst , pLast);
+    printf("\nnode count = %d\n",CountNodes(pFirst,pLast));
+
+    int FirstDeleted  = DeleteFirst(&pFirst , &pLast);
+    if(-1 != FirstDeleted){
+        printf("First data deleted %d\n", FirstDeleted);
+    }
+    Display(pFirst , pLast);
+    printf("\nnode count = %d\n",CountNodes(pFirst,pLast));
+    int LastDeleted = DeleteLast(&pFirst , &pLast);
+     if(-1 != LastDeleted){
+        printf("Last data deleted %d\n", LastDeleted);
+    }
+    Display(pFirst , pLast);
+    printf("\nnode count = %d\n",CountNodes(pFirst,pLast));
     if(pFirst != NULL){
         DeleteAll(&pFirst , &pLast);
     }
     Display(pFirst , pLast);
+    printf("\nnode count = %d\n",CountNodes(pFirst,pLast));
     return 0;
 }
 
@@ -56,7 +84,71 @@ void InsertFirst(struct node** ppHead , struct node** ppTail , int iNo){
 }
 
 void InsertLast(struct node** ppHead, struct node** ppTail , int iNo){
-    
+    struct node *pNewNode = NULL;
+    struct node* pTemp = 
+    pNewNode = (struct node*)malloc(sizeof(struct node));
+
+    if(NULL == pNewNode){
+        printf("Memory Allocation Failed..!\n");
+        return;
+    }
+
+    if(NULL == *ppHead){
+        *ppHead = pNewNode;
+        *ppTail = pNewNode;
+        (*ppTail)->pNext = *ppHead;
+        return;
+    }
+    pNewNode->iData = iNo;
+    pNewNode->pNext = (*ppTail)->pNext;
+    (*ppTail)->pNext = pNewNode;
+    *ppTail = pNewNode;
+    return;
+}
+
+int DeleteFirst(struct node** ppHead , struct node** ppTail){
+    int iDeletedData;
+    if(NULL == *ppHead){
+        return -1;
+    }
+    iDeletedData = (*ppHead)->iData;
+    *ppHead = (*ppHead)->pNext;
+    free((*ppTail)->pNext);
+    (*ppTail)->pNext = *ppHead;
+    return iDeletedData;
+}
+
+int DeleteLast(struct node** ppHead, struct node**ppTail){
+    struct node* pTemp = NULL;
+    int iDeletedData;
+    if(NULL == *ppHead){
+        return -1;
+    }
+    iDeletedData = (*ppTail)->iData;
+    pTemp = *ppHead;
+
+    while(pTemp->pNext != *ppTail){
+        pTemp = pTemp->pNext;
+    }
+    *ppTail = pTemp;
+    free((*ppTail)->pNext);
+    (*ppTail)->pNext = *ppHead;
+    return iDeletedData;
+
+}
+
+int CountNodes(struct node* pHead , struct node* pTail){
+    //struct node* pTemp = NULL;
+    int iCounter = 0;
+    if(NULL == pHead){
+        return iCounter;
+    }
+    do{
+        iCounter++;
+        pHead = pHead->pNext;
+    }while(pHead != pTail->pNext);
+
+    return iCounter;
 }
 
 void Display(struct node* pHead , struct node* pTail){
@@ -68,7 +160,10 @@ void Display(struct node* pHead , struct node* pTail){
         printf("|%d|->",pHead->iData);
         pHead = pHead->pNext;
     }while(pHead != pTail->pNext);
-    
+    printf("\n");
+}
+
+int SearchFirstOccurrence(struct node* pHead , struct node* pTail , int iKey){
 
 }
 
